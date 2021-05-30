a2adfc4e2f15e358db42a9c3c1014617216105d2
rohamm <rohambadi547@gmail.com>
2021-05-30 05:20:56 -0700

    augmented reality with opencv

---
+++ main.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+
+MIN_MATCH_COUNT = 15
+frmNumber = 0
+objDetected = False
+
+img_untold = cv2.imread('untold-ar .jpeg')
+video_untold = cv2.VideoCapture('untold-intro.mp4')
+video_untold.set(1, 854)
+
+img_untold = cv2.resize(img_untold, (720, 720))
+
+success, VideoImg = video_untold.read()
+hI, wI, cI = img_untold.shape
+VideoImg = cv2.resize(VideoImg, (wI, hI))
+webcam = cv2.VideoCapture(0)
+
+orb = cv2.ORB_create(nfeatures=1000)
+kp, desc = orb.detectAndCompute(img_untold, None)
+# img_untold = cv2.drawKeypoints(img_untold, kp, None)
+
+# if you select the best matches based on crosscheck
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+while True:
+    success2, webcamImg = webcam.read()
+    webcamImg_ar = webcamImg.copy()
+
+    if objDetected is True:
+        if frmNumber == video_untold.get(cv2.CAP_PROP_FRAME_COUNT):
+            video_untold.set(1, 0)
+            frmNumber = 0
+        success, VideoImg = video_untold.read()
+        VideoImg = cv2.resize(VideoImg, (wI, hI))
+    else:
+        video_untold.set(1, 0)
+        frmNumber = 0
+
+    kp2, desc2 = orb.detectAndCompute(webcamImg, None)
+    # webcamImg = cv2.drawKeypoints(webcamImg, kp2, None)
+    # matches = bf.match(desc, desc2)
+    # matches = sorted(matches, key=lambda x: x.distance)
+    # img3 = cv2.drawMatches(img_untold, kp, webcamImg, kp2, matches[:10], flags=2, outImg=None)
+    matches = bf.knnMatch(desc, desc2, k=2)
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append([m])
+    img3 = cv2.drawMatchesKnn(img_untold, kp, webcamImg, kp2, good, flags=2, outImg=None)
+
+    if len(good) > MIN_MATCH_COUNT:
+        objDetected = True
+        src_pts = np.float32([kp[m.queryIdx].pt for [m] in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for [m] in good]).reshape(-1, 1, 2)
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        # print(M)
+        # print(mask)
+        src_bnd_pts = np.float32([[0, 0], [0, hI], [wI, hI], [wI, 0]]).reshape(-1, 1, 2)
+        dst_bnd_pts = cv2.perspectiveTransform(src_bnd_pts, M)
+        cv2.polylines(webcamImg, [np.int32(dst_bnd_pts)], True, (255, 0, 255), thickness=2)
+
+        videoWarped = cv2.warpPerspective(VideoImg, M, (webcamImg.shape[1], webcamImg.shape[0]))
+        maskwin = np.zeros((webcamImg.shape[0], webcamImg.shape[1]), np.uint8)
+        cv2.fillPoly(maskwin, [np.int32(dst_bnd_pts)], (255, 255, 255))
+        maskwinInv = cv2.bitwise_not(maskwin)
+        webcamImg_ar = cv2.bitwise_and(webcamImg_ar, webcamImg_ar, mask=maskwinInv)
+        webcamImg_ar = cv2.bitwise_or(videoWarped, webcamImg_ar)
+
+        cv2.imshow('VideoWarped', videoWarped)
+        cv2.imshow('Mask Window', webcamImg_ar)
+    else:
+        objDetected = False
+        webcamImg_ar = webcamImg
+        cv2.imshow('Mask Window', webcamImg_ar)
+
+    # cv2.imshow('Matching', img3)
+    cv2.imshow('WebCam', webcamImg)
+    # cv2.imshow('untold Image', img_untold)
+    # cv2.imshow('untold Video', VideoImg)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    frmNumber += 5
+webcam.release()
+cv2.destroyAllWindows()
